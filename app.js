625d521415034538daae063e20142d9c169941d7
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-26 22:27:13 +0530

    implemented POST method handling in api

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+// Middleware -> a function that can modify the incoming request data
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from the server side' , app: 'Natours'})
 // })
@@ -17,6 +20,7 @@ const tours = JSON.parse(
 
 // get basically setups a GET request(method) in the api route
 
+// GET request
 app.get('/api/v1/tours', (req, res) => {
   // this function is called route handler
   res.status(200).json({
@@ -26,6 +30,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+// POST request
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  // taking id from previous object from the tours array
+  const newId = tours[tours.length - 1].id + 1;
+  // creating new Object with id and requested tour data from req.data
+  const newTour = Object.assign({ id: newId }, req.body);
+  // Pushing the new tour to tours 
+  tours.push(newTour);
+  // writing the tours file with all new tours 
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    })
+  })
+
 });
 
 const port = 3000;
